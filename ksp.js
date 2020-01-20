f21bc0a71e690c2704912be7837b785ab412cc48
mk-pmb <mk-pmb@users.noreply.github.com>
2020-01-21 00:09:47 +0100

    v1.0.1: Optimize the trivial cases

--- ksp.js
+++ ksp.js
@@ -8,12 +8,25 @@
   function getOwn(o, k, d) { return (objHas.call(o, k) ? o[k] : d); }
 
   EX = function dictLookupKeySuffixParts(dict, parts, opts) {
+    if (!dict) { return false; }
     opts = (opts || false);
     var nPartsTotal = parts.length, skip, partsUsed,
       glue = (opts.glue || ''),
       kPfx = (opts.keyPrefix || ''),
       kSuf = (opts.keySuffix || ''),
+      dKeys = Object.keys(dict), nKeys = dKeys.length,
       key, val;
+    if (nKeys === 0) { return false; }
+    if (nKeys === 1) {
+      key = kPfx + kSuf;
+      if (dKeys[0] === key) {
+        val = dict[key];
+        if (val !== undefined) {
+          return { key: key, val: val, partsUsed: [],
+            nUsed: 0, nSkip: nPartsTotal };
+        }
+      }
+    }
     for (skip = 0; skip <= nPartsTotal; skip += 1) {
       partsUsed = parts.slice(skip);
       key = kPfx + partsUsed.join(glue) + kSuf;
